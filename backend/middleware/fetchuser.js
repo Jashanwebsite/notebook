623a6c0d64b3b485046f8e4d7e159e8c39e3154b
Jashanwebsite <jashandeepsingh61124@gmail.com>
2024-01-17 19:31:56 +0530

    mongo uri change

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -2,7 +2,7 @@ var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'Harryisagoodb$oy';
 
 const fetchuser = (req, res, next) => {
-    // Get the user from the jwt token and add id to req object
+    // Get the user from the jwt tokenand add id to req object
     const token = req.header('auth-token');
     if (!token) {
         res.status(401).send({ error: "Please enter token authenticate using a valid token" })
